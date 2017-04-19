c3df6d5a446455a9524645944ae2468d6e418834
Koray <koraykupe@gmail.com>
2017-04-19 15:58:25 +0300

    add setUp function

--- tests/Auth/BasicAuthTest.php
+++ tests/Auth/BasicAuthTest.php
@@ -5,12 +5,17 @@ use PHPUnit\Framework\TestCase;
 
 class BasicAuthTest extends TestCase
 {
+    private $userRepository;
+    private $session;
+
+    public function setUp() {
+        $this->userRepository = $this->getMockBuilder('JobBoard\Repositories\UserRepository')->getMock();
+        $this->session = $this->getMockBuilder('JobBoard\Session\Session')->getMock();
+    }
     /** @test */
     public function get_user_returns_null_when_not_logged_in()
     {
-        $userRepository = $this->getMockBuilder('JobBoard\Repositories\UserRepository')->getMock();
-        $session = $this->getMockBuilder('JobBoard\Session\Session')->getMock();
-        $auth = new \JobBoard\Auth\BasicAuth($userRepository, $session);
+        $auth = new \JobBoard\Auth\BasicAuth($this->userRepository, $this->session);
         $this->assertNull($auth->getUser());
     }
 }
